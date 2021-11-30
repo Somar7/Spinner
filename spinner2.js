a43d31daf3f59de011f58f0b65b15f58d7f7b8a5
somar7 <somar7@nait.ca>
2021-11-30 23:17:17 +0000

    Refactored spinner exercise

--- spinner2.js
+++ spinner2.js
@@ -1,15 +1,10 @@
-let symbols = "|/-\|/-\|";
-let time = 100;
-let x = 0;
-for (const char of symbols) {
-  setTimeout(() => {
-    symbols[x] = char
-    x++;
-    if (x !== symbols.length) {
-      process.stdout.write(`\r${char}    `);
-    } else {
-      process.stdout.write(`\r${char}   \n`);
-    }
-  }, time += 200)
+let symbols = "|/-\|/-\| \n";
+let delay = 0;
+let increment = 500;
 
-}
+for(const char of symbols) {
+  setTimeout(() => {
+    process.stdout.write("\r" + char);
+  }, delay);
+  delay += increment;
+};
